bee5966d826b5873b336ab879c251853dc4e4be2
Atharva Pardeshi <atharva.exe@gmail.com>
2023-03-13 11:41:37 +0530

    refactor: use range to iterate through numbers

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,8 +2,10 @@ package main
 
 func Sum(numbrers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbrers[i]
+
+	for _, number := range numbrers {
+		sum += number
 	}
+
 	return sum
 }
