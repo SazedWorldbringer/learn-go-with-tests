b85b2a13f4115d56d405268972bec23c2fa54107
Atharva Pardeshi <atharva.exe@gmail.com>
2023-03-22 17:32:56 +0530

    test: write test for function evaluating a circles area chore: define Circle type

--- 05-structs/shapes_test.go
+++ 05-structs/shapes_test.go
@@ -8,16 +8,28 @@ func TestPerimeter(t *testing.T) {
 	want := 40.0
 
 	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+		t.Errorf("got %.2f, want %.2f", got, want)
 	}
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := Area(rectangle)
+		want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
+	})
 }
