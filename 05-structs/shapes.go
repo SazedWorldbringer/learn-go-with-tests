11ee54ea6b3371d9422cc29437a202a8bae6b344
Atharva Pardeshi <atharva.exe@gmail.com>
2023-03-25 22:52:48 +0530

    feat: add Triangle struct, write Area method for Triangle

--- 05-structs/shapes.go
+++ 05-structs/shapes.go
@@ -11,6 +11,15 @@ type Rectangle struct {
 	Height float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
@@ -19,10 +28,10 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-type Circle struct {
-	Radius float64
-}
-
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
