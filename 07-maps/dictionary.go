95308c9b7af3b67661f0b2b412d5ff1b656629ee
Atharva Pardeshi <atharva.exe@gmail.com>
2023-07-28 12:57:47 +0530

    feat: handle words that don't exist for update method

--- 07-maps/dictionary.go
+++ 07-maps/dictionary.go
@@ -5,8 +5,9 @@ type Dictionary map[string]string
 type DictionaryErr string
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 func (e DictionaryErr) Error() string {
@@ -36,9 +37,23 @@ func (d Dictionary) Add(word, definition string) error {
 	default:
 		return err
 	}
+
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		// return error if word doesn't exist in the dictionary
+		return ErrWordDoesNotExist
+	case nil:
+		// update definition if word exists in the dictionary
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
 }
